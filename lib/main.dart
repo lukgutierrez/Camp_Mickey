360311a9ad66bf19546eb70088af63c7bb5f692c
Luk Gutierrez <lucianogutierrezagustin@gmail.com>
2023-03-23 11:23:03 -0300

    Comera_Fuction

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,109 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:camera/camera.dart';
+import 'package:camp_mikey/page/Home_Page.dart';
+import 'package:camp_mikey/page/Save_Image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_isolate/flutter_isolate.dart';
-import 'package:path_provider/path_provider.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() => runApp(MyApp());
 
-  // Inicializar la cámara
-  final cameras = await availableCameras();
-  final firstCamera = cameras.first;
-  final camera = await CameraController(
-    firstCamera,
-    ResolutionPreset.high,
-  ).initialize();
-
-  runApp(MyApp(camera: camera));
-}
-
-class MyApp extends StatefulWidget {
-  final CameraController camera;
-
-  MyApp({required this.camera});
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  void takePicturesInIsolate() async {
-    final isolate = await FlutterIsolate.spawn(_takePictures, null);
-    // Espera a que se tomen todas las fotos antes de mostrar el mensaje
-    await isolate.done;
-    print('¡Se tomaron todas las fotos!');
-  }
-
-  static Future<void> _takePictures(_) async {
-    for (var i = 0; i < 10; i++) {
-      final Directory directory = await getTemporaryDirectory();
-      final String filePath = '${directory.path}/image_$i.jpg';
-
-      // Tomar foto y guardarla en el directorio temporal
-      await Future.delayed(Duration(seconds: 1));
-      await widget.camera.takePicture(filePath);
-    }
-  }
-
-  @override
-  void dispose() {
-    widget.camera.dispose();
-    super.dispose();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Background Camera Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Background Camera Demo'),
-        ),
-        body: Center(
-          child:
-              Text('Presione el botón para tomar 10 fotos en segundo plano.'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            takePicturesInIsolate();
-          },
-          child: Icon(Icons.camera),
-        ),
-      ),
-    );
+        theme: ThemeData(primaryColor: Colors.red),
+        debugShowCheckedModeBanner: false,
+        title: 'CAMPMICKEY',
+        home: HomePage());
   }
 }
+
+
+// import 'dart:async';
+// import 'dart:io';
+
+// import 'package:camera/camera.dart';
+// import 'package:camera/camera.dart';
+// import 'package:camera/camera.dart';
+// import 'package:flutter/material.dart';
+// import 'package:path_provider/path_provider.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+
+//   // Inicializar la cámara
+//   final cameras = await availableCameras();
+//   final firstCamera = cameras.first;
+//   final camera = await CameraController(
+//     firstCamera,
+//     ResolutionPreset.high,
+//   ).initialize();
+
+//   runApp(MyApp(camera: camera));
+// }
+
+// class MyApp extends StatefulWidget {
+//   final CameraController camera;
+
+//   MyApp({required this.camera});
+
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   int _photoCount = 0;
+
+//   void _takePhoto() async {
+//     // Obtener el directorio de almacenamiento externo
+//     final Directory? extDir = await getExternalStorageDirectory();
+//     final String dirPath = '${extDir?.path}/Pictures/flutter_camera_background';
+//     await Directory(dirPath).create(recursive: true);
+
+//     // Generar el nombre de archivo para la foto
+//     final String filePath = '$dirPath/${DateTime.now().millisecondsSinceEpoch}.jpg';
+
+//     // Tomar la foto
+//     try {
+//       await widget.camera.takePicture(filePath);
+//       setState(() {
+//         _photoCount++;
+//       });
+//     } catch (e) {
+//       print('Error al tomar la foto: $e');
+//     }
+//   }
+
+//   @override
+//   void dispose() {
+//     widget.camera.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Background Camera Demo',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Flutter Background Camera Demo'),
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text('Presione el botón para tomar 10 fotos automáticamente.'),
+//               SizedBox(height: 16),
+//               Text('$_photoCount fotos tomadas.'),
+//             ],
+//           ),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           onPressed: () {
+//             for (var i = 0; i < 10; i++) {
+//               _takePhoto();
+//             }
+//           },
+//           child: Icon(Icons.camera),
+//         ),
+//       ),
+//     );
+//   }
+// }
